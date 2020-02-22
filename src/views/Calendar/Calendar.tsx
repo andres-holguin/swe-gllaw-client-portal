a402afc91ceebb28870435365ab4f7880db740a9
Anna <annalanzino@ufl.edu>
2020-02-22 13:09:45 -0500

    added calendar component

--- src/views/Calendar/Calendar.tsx
+++ src/views/Calendar/Calendar.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import './main.scss'
+
+// can add events could be a prop passed to Calendar so if they are an admin they can add events
 
 const Calendar = () => {
     return (
         <div>
-            <text>hello</text>
+            <FullCalendar defaultView="dayGridMonth" plugins={[ dayGridPlugin ]} />
         </div>
     )
 }
