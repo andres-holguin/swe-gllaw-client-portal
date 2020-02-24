c6b1c110af8c39556a2cb833907791103b0e6283
Connor Bolick <cbolick314@gmail.com>
2020-02-24 15:45:34 -0500

    Second Draft
    Added progress array.

--- src/ListingModel.ts
+++ src/ListingModel.ts
@@ -4,7 +4,8 @@ const listingSchema = new mongoose.Schema({
   username: {type : String, required: true},
   email: {type : String, required: true},
   password: {type : String, required: true},
-  isAdmin: {type : Boolean}
+  isAdmin: {type : Boolean, required: true},
+  //progress: {type : Array, required: true}
 });
 
 export default mongoose.model('listings', listingSchema);
