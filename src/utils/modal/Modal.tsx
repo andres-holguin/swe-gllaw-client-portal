9c8e0c3e0aa59178af750db4d1af2173445b61de
Anna <annalanzino@ufl.edu>
2020-02-24 11:23:02 -0500

    added the modal- need to figure out how to overlay

--- src/utils/modal/Modal.tsx
+++ src/utils/modal/Modal.tsx
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
-// import TimePicker from 'react-time-picker';
+import TimePicker from 'react-time-picker';
 import { MdClear } from 'react-icons/md';
 
 import './Modal.css'
 
 interface Props {
     visible: boolean,
-    date: Date,
+    date: any,
     closeModal: any
 }
 
 const Modal: React.FC<Props> = (props) => {
     const [event, setEvent] = useState('')
+    const [time, setTime] = useState('')
 
     return (
         <div>
@@ -19,11 +20,16 @@ const Modal: React.FC<Props> = (props) => {
                 props.visible ? 
                 (
                     <div className='modal'>
-                        <div className='button'>
-                            <MdClear
-                                onClick={props.closeModal}
-                                className='exit'
-                            />
+                        <div className='modalHeader'>
+                            <div className='date'>
+                                {props.date.dateStr}
+                            </div>
+                            <div className='exitButton'>
+                                <MdClear
+                                    onClick={props.closeModal}
+                                    className='exit'
+                                />
+                            </div>
                         </div>
                         <div>
                             <form>
@@ -35,12 +41,20 @@ const Modal: React.FC<Props> = (props) => {
                                 />
                             </form>
                         </div>
-                        {/* <div>
-                            <TimePicker />
-                        </div> */}
+                        <TimePicker
+                            className='timePicker'
+                            isOpen={true}
+                            onChange={(time) => setTime(time)}
+                            clockIcon={null}
+                            clearIcon={null}
+                            disableClock={true}
+                        />
+                        <div className='addButton'>
+                            <button className='addButtonStyle'>Add Event</button>
+                        </div>
                     </div>
                 ) : (
-                <text>not visible</text> 
+                <></> 
                 )
             }
         </div>
