024627f47a12080cf0379306c1db3be80ccc67ad
Yashiyah Sylvester <sylvestery@ufl.edu>
2020-02-26 12:05:39 -0500

    updated formatting

--- server/models/USERModel.ts
+++ server/models/USERModel.ts
@@ -8,4 +8,4 @@ const userSchema = new mongoose.Schema({
   //progress: {type : Array, required: true}
 });
 
-export default mongoose.model('User', userSchema);
+export default mongoose.model('users', userSchema);
