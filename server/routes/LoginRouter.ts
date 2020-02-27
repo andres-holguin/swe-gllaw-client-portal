2a4413f466c7ef3a66389ef5817f67a337f7c196
Yashiyah Sylvester <sylvestery@ufl.edu>
2020-02-27 17:20:53 -0500

    called register directly

--- server/routes/LoginRouter.ts
+++ server/routes/LoginRouter.ts
@@ -1,17 +1,13 @@
 
-import User from "../models/USERModel"
 import {create, read, update} from "../controllers/UserController"
-
+import {login, register} from "../user"
 const express = require("express");
 const loginRouter = express.Router();
 
 
-loginRouter.post("/register", (req, res) => {
-    create(req, res);
-});
-
+loginRouter.post("/register", register);
 loginRouter.post("/login", (req, res) => {
-    read(req, res);
+    login(req, res);
 });
 
 loginRouter.put("/update", (req, res) => {
