c672b069ccd8f9a6f4cf82a078db2339bdb84e29
Yashiyah Sylvester <sylvestery@ufl.edu>
2020-03-03 14:22:50 -0500

    Can create and read new users

--- server/user.ts
+++ server/user.ts
@@ -1,22 +1,29 @@
-import {read, UserRequest} from "./controllers/UserController";
-
+import {create, read, UserRequest} from "./controllers/UserController";
 
 export const login = (req, res) => {
-    /* Verify function */
-    let u_req = req.u_req;
+    let u_req = req.body.u_req;
+    console.log(u_req);
     verify(u_req);
+    req.user = req.body.u_req;
+    read(req, res);
 }
 
 export const register = (req, res) => {
+    let u_req = req.body.u_req;
+    console.log(u_req);
 
+    create(req, res);
+    res.send("User created");
 }
 
+    /* Verify function */
 const verify = (user: UserRequest) => {
-    let err:any = {};
+    let err:any;
     if (!user) {
-        err.user = 'No request was made.'
+        console.log("aaa");
+        err.user = 'No request was made.';
     }
-    
 
     if (err) throw err;
 }
+export {}
