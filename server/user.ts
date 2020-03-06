c49eca146f09e8c3bf74d0569454bf25c6415c3e
Connor Bolick <cbolick314@gmail.com>
2020-03-06 18:17:11 -0500

    unstable:: WIP Password checking

--- server/user.ts
+++ server/user.ts
@@ -1,9 +1,17 @@
 import {create, read, UserRequest} from "./controllers/UserController";
 
 export const login = (req, res) => {
+    //u_req is ?
     let u_req = req.body.u_req;
     console.log(u_req);
-    verify(u_req);
+    if(verify(u_req))
+    {
+        res.send(1);
+    }
+    else
+    {
+        res.send(0);
+    }
     req.user = req.body.u_req;
     read(req, res);
 }
@@ -11,19 +19,19 @@ export const login = (req, res) => {
 export const register = (req, res) => {
     let u_req = req.body.u_req;
     console.log(u_req);
-
     create(req, res);
     res.send("User created");
 }
 
-    /* Verify function */
+/* Verify function */
 const verify = (user: UserRequest) => {
+    //password verifcation goes on in here
     let err:any;
     if (!user) {
         console.log("aaa");
         err.user = 'No request was made.';
     }
-
     if (err) throw err;
+    return true;
 }
 export {}
