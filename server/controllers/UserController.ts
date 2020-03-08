e5df18c648f6eaf0a788fdce6d997c2172932872
Yashiyah Sylvester <sylvestery@ufl.edu>
2020-03-07 19:10:35 -0500

    Added password hashing

--- server/controllers/UserController.ts
+++ server/controllers/UserController.ts
@@ -2,10 +2,18 @@
 import User from '../models/USERModel';
 
 const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
 export interface UserRequest {
     email: String,
     userName: String,
     password: String,
+}
+
+interface newPasswordRequest {
+    username: string,
+    oldPassword: string,
+    newPassword: string,
 }
 
 /* Create a listing */
@@ -13,7 +21,10 @@ export const create = async (req, res) => {
     let err:any = {};
     let user_req: UserRequest = req.body.u_req;
     console.log(user_req);
+    let hashedPassword = bcrypt.hashSync(user_req.password, saltRounds);
+    console.log(hashedPassword);
     User.findOne({username: user_req.userName}).then(user => {
+        
         if (user){
             console.log("User already exisit");
             err.user = "User already exists"; //TODO send back a response.
@@ -21,7 +32,7 @@ export const create = async (req, res) => {
             User.create({ 
                 username: user_req.userName,
                 email: user_req.email,
-                password: user_req.password,
+                password: hashedPassword,
                 isAdmin: false,
             }).then( (d)  => {
                 d.save();
@@ -55,34 +66,75 @@ export const verifyUser = (req, res) => {
         } else {
             console.log(u); 
             console.log(user.toObject());
-            if (u.password === user.toObject().password) {
+            let hash = user.toObject().password;
+            if (bcrypt.compareSync(u.password, hash)) {
+            
                 console.log("user confirmed");
                 res.send(true);
             } else {
+                console.log("User not confirmed");
                 res.send(false);
             }
         }
     });
 }
 /* Update a listing*/
-export const update = (req, res) => {
-/*   User.findOneAndUpdate({userName: req.user.UserName}, (err, result) => {
-       if (err) throw err;
-       console.log(update)
-   } */
-  // User.findOneAndUpdate({userName: req.user_req.userName}).
-};
+/*export const update = (req, res) => {
+    let u_req: UserRequest = req.body.u_req;
+   User.findOneAndUpdate({username: u_req.userName}, {
+       username: req.user.username
+   }
+}; */
+
+const update = (updated: UserRequest) => {
+
+}
+export const resetPassword = (hmm: newPasswordRequest) => {
+    let user = hmm.username;
+    //let userDocument  = getUser(user);
+    User.updateOne({username: user}, {
+        
+    })
+}
 
 /* Delete a listing */
 export const remove = (req, res) => {
-    User.findOneAndDelete({userName: req.user.userName}, (err, result) => {
+    User.findOneAndDelete({username: req.body.u_req.userName}, (err, result) => {
         if (err) throw err;
         if (!result) {
             console.log("No user by that name exist.")
+            res.status(404).json({nouser: "No user by that name exist."});
         }
         
     });
 };
+
+export const getUser = (username: string) => {
+    User.findOne({username: username}).exec().then( (d) => {
+        return d;
+    })
+}
+
+const isAdmin = (username: string) => { //Replace this function
+    User.findOne({username: username}, (err, u) => {
+        if (err) throw err;
+        if (!u) {
+            console.log("No account with that name exist.");
+            return true;
+        } else {
+            if(u.toObject().isAdmin) {
+                console.log("is admin.");
+                return true;
+            } else {
+                return false;
+            }
+        }
+    });
+}
+const deleteUser = (req, res) => {
+    //Im gonna need to check if the user is an admin.
+    //req.body.u_req;
+}
 
 /* Retreive all the directory listings*/
 export const list = (req, res) => {
