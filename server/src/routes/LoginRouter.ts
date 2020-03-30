b07045975ee611e3f54480250113a0e035e8d312
Connor Bolick <cbolick314@gmail.com>
2020-03-29 20:27:46 -0400

    Framework

--- server/src/routes/LoginRouter.ts
+++ server/src/routes/LoginRouter.ts
@@ -15,10 +15,23 @@ loginRouter.put("/update", (req, res) => {
     update(req, res);
 });
 
+loginRouter.get("/Calender", (req, res) => {
+    
+
+
+});
+
+loginRouter.post("/Calender", (req, res) => {
+
+
+    
+});
+
+loginRouter.delete("/Calender", (req, res) => {
 
 
 
-
+});
 
 module.exports = loginRouter;
 export default loginRouter;
