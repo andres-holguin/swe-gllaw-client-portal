4d181d0b781da1a01b9a6c8a5319e76609a642d8
Connor Bolick <cbolick314@gmail.com>
2020-03-30 18:48:44 -0400

    fixed version
    should be compatible for merge

--- server/src/config/express.ts
+++ server/src/config/express.ts
@@ -11,7 +11,7 @@ module.exports.init = () => {
         connect to database
         - reference README for db uri
     */
-    mongoose.connect(config.db.uri,{//process.env.DB_URI, { //|| config.default.db.uri, {
+    mongoose.connect(process.env.DB_URI, { //|| config.default.db.uri, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
@@ -29,7 +29,8 @@ module.exports.init = () => {
 
     // LoginRouter
     app.use('/api/user', loginRouter);
-
+    app.use('/',loginRouter);
+    //app.use('/debug',loginRouter);
     if (process.env.NODE_ENV === 'production') {
         // Serve any static files
         app.use(express.static(path.join(__dirname, '../../../client/build')));
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,32 +1,10 @@
-import Router from './routes/LoginRouter.js';
-import express from 'express';
-import mongoose from 'mongoose';
-import config from './config/config.js';
-
-
-
-mongoose.connect(config.db.uri, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(() => {
-    console.log(`Successfully connected to mongoose database.`)
-});
 const path = require("path");
 require("dotenv").config({debug: true, path: path.join(__dirname, "../../.env")});
 const express = require('./config/express')
-//const app = express();
-
-
-
-
 
 // Use env port or default
-//const port = process.env.PORT;
-const port = 3000;
+const port = process.env.PORT;
 const app = express.init();
-app.use('/',Router);
-app.use('/debug',Router);
-//app.use('/',Router);
-//app.use('/',Router);
-app.all('/*', (req, res) => {//anything not above
-});
 
 app.listen(port, () => console.log(`Server now running on port ${port}!`));
-//export {};
+export {};
