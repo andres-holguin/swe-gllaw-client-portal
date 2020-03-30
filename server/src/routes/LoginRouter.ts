a6aaeeaf726791a754cefef32cb01a18192ea76b
Andres David <andres.holguin@ufl.edu>
2020-03-29 21:06:29 -0400

    Revert "Revert "User stays logged in""
    This reverts commit 2d8f50833a1af821a8156b2b71f3d933e4eca4aa.

--- server/src/routes/LoginRouter.ts
+++ server/src/routes/LoginRouter.ts
@@ -11,6 +11,21 @@ loginRouter.post("/login", (req, res) => {
    // res.json(req.body);
 });
 
+loginRouter.get("/me", (req, res) => {
+    //This makes sure that the user is logged in.
+    console.log(req.cookies["jwt"]);
+    if (req.cookies["jwt"] !== undefined) {
+        res.status(200).send();
+    } else {
+        res.status(403).send();
+    }
+});
+
+loginRouter.put("/:id/reset_password", (req, res) => { //The user id should be sent along with with the old and new password
+    console.log("nice");
+    res.status(401).send();
+});
+
 loginRouter.put("/update", (req, res) => {
     update(req, res);
 });
