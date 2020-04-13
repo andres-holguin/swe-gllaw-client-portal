f1518c818c7bd974a2ad3c3d03db9ba833dba875
jonesluke1999 <59861784+jonesluke1999@users.noreply.github.com>
2020-04-13 15:28:43 -0400

    Update List.tsx

--- client/src/components/List/List.tsx
+++ client/src/components/List/List.tsx
@@ -5,17 +5,38 @@ import ListGroup from 'react-bootstrap/ListGroup';
 interface Props {
     data: any,
     target: String
+    isAdmin: boolean
 }
 
 const List: React.FC<Props> = (props) => {
     const res = props.data//.filter(element => element.name.includes(props.target));
 
-    return (
+    if(props.isAdmin)return (
         <div>
             <ListGroup as='ul'variant='flush'>
                 {res.map((el, index) =>
                 <ListGroup.Item action as='li'>
                     {index}. {el}
+                    <div>
+                        <button>View</button>
+                        <button>Download</button>
+                        <button>Delete</button>
+                    </div>
+                </ListGroup.Item>
+                )}
+            </ListGroup>
+        </div>
+    )
+    else return (
+        <div>
+            <ListGroup as='ul'variant='flush'>
+                {res.map((el, index) =>
+                <ListGroup.Item action as='li'>
+                    {index}. {el}
+                    <div>
+                        <button>View</button>
+                        <button>Download</button>
+                    </div>
                 </ListGroup.Item>
                 )}
             </ListGroup>
