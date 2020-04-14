e5a9c2d368e6a2c1fbd2e493a9db6c57d2b271b6
Daniel Alvarez <32178126+danieldan99@users.noreply.github.com>
2020-04-14 10:03:38 -0400

    Finished progress bar and login

--- client/src/components/Header/NavBar.tsx
+++ client/src/components/Header/NavBar.tsx
@@ -1,12 +1,70 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import './NavBar.css';
 import ProgressBar from '../ProgressBar/ProgressBar'
 import logo from '../../images/GonzaloLawLogo.png';
 
+
+
+
+
+
 const NavBar = () => {
     const [progressPercent, setProgressPercent] = useState(10)
+
+    let previousBtn; 
+    let nextBtn; 
+    let content; 
+    let bullets; 
+
+useEffect(() => {previousBtn= document.getElementById('previousBtn');
+
+                nextBtn = document.getElementById('nextBtn');
+                content  = document.getElementById('content');
+                bullets  = [...document.querySelectorAll('.bullet')];
+            }, []);
+
+
+
+const MAX_STEPS = 7;
+let currentStep = 1;
+
+       const next = () => {
+    const currentBullet = bullets[currentStep - 1];
+    console.log("Here");
+    currentBullet.classList.add('completed');
+    console.log(bullets);
+    currentStep++;
+    previousBtn.disabled = false;
+    if(currentStep === MAX_STEPS){
+        currentBullet.classList.remove('completed');
+        currentBullet.classList.add('finalstep');
+        nextBtn.disabled = true;
+    }
+
+
+       }
+
+      const prev = () => {
+   const previousBullet = bullets[currentStep - 2];
+   previousBullet.classList.remove('finalstep');
+   previousBullet.classList.remove('completed');
+   currentStep--;
+   nextBtn.disabled = false;
+   if(currentStep === 1){
+    previousBtn.disabled = true;
+   }
+   
+
+
+
+}
+
+
+
+
+
 
     return (
         <div>
@@ -22,8 +80,51 @@ const NavBar = () => {
                 </div>
             </div>
             <div className='progressBar'>
-                <ProgressBar percent={progressPercent}/>
-                <button onClick={() => setProgressPercent(prev => prev + 10)}>update progress</button>
+
+                <div id="stepProgressBar">
+                    <div className="step">
+                        <h6 className="step-text"> IP </h6>
+                        <div className="bullet" id = "1"> 1 </div>
+                        
+                    </div>
+
+                    <div className="step">
+                        <h6 className="step-text"> Initial Intake </h6>
+                        <div className="bullet"> 2 </div>
+                    </div>
+                    <div className="step">
+                        <h6 className="step-text">Initial Search</h6>
+                        <div className="bullet">3</div>
+                    </div>
+                    <div className="step">
+                        <h6 className="step-text">Initial Filing</h6>
+                        <div className="bullet">4</div>
+                    </div>
+                    <div className="step">
+                        <h6 className="step-text">Client Follow Up</h6>
+                        <div className="bullet">5</div>
+                    </div>
+                    <div className="step">
+                        <h6 className="step-text">Final Outcome</h6>
+                        <div className="bullet">6</div>
+                    </div>
+
+                    <div id="main">
+                    
+                        <button id="previousBtn" onClick = {prev} >Previous</button>
+                        <button id="nextBtn" onClick = {next} >Next</button>
+                       
+
+                    </div>
+
+
+
+
+                </div>
+
+
+                
+
             </div>
         </div>
     )
