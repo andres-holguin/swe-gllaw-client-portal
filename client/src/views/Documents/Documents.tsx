5759dad36a6a0be040970212470386a315a5ed58
jonesluke1999 <59861784+jonesluke1999@users.noreply.github.com>
2020-04-19 13:09:29 -0400

    Update Documents.tsx

--- client/src/views/Documents/Documents.tsx
+++ client/src/views/Documents/Documents.tsx
@@ -1,40 +1,61 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import NavBar from '../../components/Header/NavBar';
 import List from '../../components/List/List'
 import axios from 'axios';
 import { useForm } from 'react-hook-form';
 import { fileURLToPath } from 'url';
 import './Documents.css'
+import { stringify } from 'querystring';
+
+
 
 const Documents = () => {
-    const { register, handleSubmit, errors, reset, getValues } = useForm();
-    const [listEntries, setListEntries] = useState([
-        "Document A",
-        "Document B",
-        "Document C"
-    ]);
+    var caseIndex = parseInt(sessionStorage.getItem('caseIndex')||'');
+    let caseData = {
+        id: "",
+        documents: [] }
+
+    useEffect(() => {
+        async function loadData() {
+            const meResponse = await axios.get("/api/auth/me");
+            setAdmin(meResponse.data.admin);
+
+            const caseResponse = await axios.get("/api/user/cases");
+            caseData = caseResponse.data.cases[caseIndex];
+            setListEntries(caseData.documents);
+            setCaseId(caseData.id);
+        }
+
+        loadData();
+    }, [])
+
+    const {register, handleSubmit, errors, reset, getValues } = useForm();
+    const [listEntries, setListEntries] = useState(caseData.documents);
+    const [isAdmin, setAdmin] = useState(false);
+    const [caseId, setCaseId] = useState("");
     
     const onSubmit = () =>{
         const document = getValues().datafile[0];
-       // console.log(document);
+    // console.log(document);
 
         const data = new FormData();
         data.append('doc', document);
-        data.append('caseid', ''); // THis will be gotten from the active case id.
+        data.append('CaseID', caseId); // THis will be gotten from the active case id.
         console.log(data);
         axios.post('/api/document/upload', data)
-          .then(function (response) {
+        .then(function (response) {
             console.log(response);
-          })
-          .catch(function (error) {
+        })
+        .catch(function (error) {
             console.log(error);
-          });
+        });
         reset();
+        window.location.reload(true);
     }
-    return (
+    if(isAdmin)return (
         <div>
             <NavBar />
-            <List data={listEntries} target="" isAdmin = {true}/>
+            <List data={listEntries} target="" isAdmin = {isAdmin}/>
             <form className = "submission-form" onSubmit = {handleSubmit(onSubmit)} encType="multipart/form-data"/*action='/api/document/upload' encType="multipart/form-data" method="post"*/>
                 <h5>
                     Upload Document <br></br>
@@ -46,7 +67,13 @@ const Documents = () => {
                 </div>
             </form>
         </div>
-    )
+    );
+    else return (
+        <div>
+            <NavBar />
+            <List data={listEntries} target="" isAdmin = {isAdmin}/>
+        </div>
+    );
 }
 
 export default Documents
