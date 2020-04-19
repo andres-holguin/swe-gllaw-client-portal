dde45a06a88515f0aa3021bab37bdefc98585941
jonesluke1999 <59861784+jonesluke1999@users.noreply.github.com>
2020-04-19 13:07:58 -0400

    Update List.tsx

--- client/src/components/List/List.tsx
+++ client/src/components/List/List.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Data from './Data';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Axios from 'axios';
+import createObjectURL from 'url';
+
 
 interface Props {
     data: any,
@@ -8,40 +11,56 @@ interface Props {
     isAdmin: boolean
 }
 
-const List: React.FC<Props> = (props) => {
-    const res = props.data//.filter(element => element.name.includes(props.target));
+const viewDocument = (id, name) => {
+    //let document = new File(["foo"], "foo.txt", {type: "text/plain"});
+    Axios.get('/api/document/' + id + '/', {responseType: 'arraybuffer'}).then((res)=>{
+            var file = new Blob([res.data], {type: 'application/pdf'});
+            const fileUrl = URL.createObjectURL(file);
+            window.open(fileUrl);
+    });
+    // const fileUrl = URL.createObjectURL(document);
+    // window.open(fileUrl, '_blank');
+}
 
-    if(props.isAdmin)return (
-        <div>
-            <ListGroup as='ul'variant='flush'>
-                {res.map((el, index) =>
-                <ListGroup.Item action as='li'>
-                    {index}. {el}
-                    <div>
-                        <button>View</button>
-                        <button>Download</button>
-                        <button>Delete</button>
-                    </div>
-                </ListGroup.Item>
-                )}
-            </ListGroup>
-        </div>
-    )
-    else return (
-        <div>
-            <ListGroup as='ul'variant='flush'>
-                {res.map((el, index) =>
-                <ListGroup.Item action as='li'>
-                    {index}. {el}
-                    <div>
-                        <button>View</button>
-                        <button>Download</button>
-                    </div>
-                </ListGroup.Item>
-                )}
-            </ListGroup>
-        </div>
-    )
+const List: React.FC<Props> = (props) => {
+    const res = props.data;//.filter(element => element.name.includes(props.target));
+    if(props.data){
+        if(props.isAdmin)return (
+            <div>
+                <ListGroup as='ul'variant='flush'>
+                    {res.map((el, index) =>
+                    <ListGroup.Item action as='li'>
+                        {index}. {el.Name}
+                        <div>
+                            <button onClick = {(e) => {viewDocument(el.fileID, el.Name)}}>Open</button>
+                        </div>
+                    </ListGroup.Item>
+                    )}
+                </ListGroup>
+            </div>
+        )
+        else return (
+            <div>
+                <ListGroup as='ul'variant='flush'>
+                    {res.map((el, index) =>
+                    <ListGroup.Item action as='li'>
+                        {index+1}. {el.Name}
+                        <div>
+                            <button onClick = {(e) => {viewDocument(el.fileID, el.Name)}}>Open</button>
+                        </div>
+                    </ListGroup.Item>
+                    )}
+                </ListGroup>
+            </div>
+        )
+    }
+    else{
+        return (
+            <div>
+                <p>No Documents</p>
+            </div>
+        )
+    }
 }
 
 export default List
